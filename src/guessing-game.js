b2573fd92e18d01e805da264271c9443bc139178
LAPTOP-HMDMPT5G\Katerina <t.ekaterina.y@gmail.com>
2017-01-30 20:20:59 +0300

    guessing-game implementation

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,27 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
+		this.max = max;  
+		this.min = min;  
 
     }
 
     guess() {
+		
+		this.value = Math.round(this.min + Math.round(this.max - this.min) / 2);
+		return this.value; 
+		
+		
 
     }
 
     lower() {
-
+		this.max = this.value; 
     }
 
     greater() {
-
+		this.min = this.value; 
+		this.max = this.max
     }
 }
 
